b369272fab74a9f8fb42ae51f6847cc421319fa8
AdityaChaudhari03 <chaudhariaditya664@gmail.com>
2024-07-11 19:09:18 +0530

    Updated

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,6 @@
 import axios from "axios"
-const API_URL = "http://localhost:8000/api-v1";
+// const API_URL = "http://localhost:8000/api-v1";
+const API_URL = "https://gethired-whfj.onrender.com/api-v1";
 
 export const API = axios.create({
     baseURL: API_URL,
